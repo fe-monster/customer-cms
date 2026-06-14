d955e06a1891390055d966652b1d8c7a7b1ada36
fe-monster <104848865+fe-monster@users.noreply.github.com>
2026-06-14 15:21:55 +0300

    feat: backend complete

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CustomerViewSet
 
-urlpatterns = [
-    path('api/', include('customers.urls')),
-]
+router = DefaultRouter()
+router.register(r'customers', CustomerViewSet)
+
+urlpatterns = router.urls
